86231e059e438a3434ed640a1f8160d4c318ce60
lucasgmartins <lucas.gmmartins@gmail.com>
2015-04-11 18:23:35 -0300

    [BACK-END] Changes API

--- server/src/main/java/io/redspark/ireadme/IRApplication.java
+++ server/src/main/java/io/redspark/ireadme/IRApplication.java
@@ -1,26 +1,14 @@
 package io.redspark.ireadme;
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
 
+@EnableJSONDoc
 @SpringBootApplication
 public class IRApplication  {
 	
-	private static final String API_PATTERN = "/api/*";
- 
 	public static void main(String[] args) {
 		SpringApplication.run(IRApplication.class, args);
-	}
-	
-	@Bean
-	public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
-	    
-		ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
-	    registration.addUrlMappings(API_PATTERN);
-	   
-	    return registration;
 	}
 }
