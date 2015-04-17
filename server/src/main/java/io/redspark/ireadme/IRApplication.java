b89773de7af0bd06e35f0a1dbf58febc94e401d3
lucasgmartins <lucas.gmmartins@gmail.com>
2015-04-17 01:03:36 -0300

    [BACK-END] Add http put filter

--- server/src/main/java/io/redspark/ireadme/IRApplication.java
+++ server/src/main/java/io/redspark/ireadme/IRApplication.java
@@ -3,6 +3,8 @@ package io.redspark.ireadme;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HttpPutFormContentFilter;
 
 @EnableJSONDoc
 @SpringBootApplication
@@ -10,5 +12,10 @@ public class IRApplication  {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(IRApplication.class, args);
+	}
+	
+	@Bean
+	public HttpPutFormContentFilter httpPutFormContentFilter() {
+		return new HttpPutFormContentFilter();
 	}
 }
